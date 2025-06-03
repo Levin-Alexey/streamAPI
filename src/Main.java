1f82127953cf85c98797e4ddc0cb76f4aa50b235
Levin-Alexey <98654124+Levin-Alexey@users.noreply.github.com>
2025-06-03 09:58:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Создаём список чисел
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
         List<Integer> positiveNumbers = new ArrayList<>();
